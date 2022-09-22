440475df9c7cf2b0c89588c829d1e089fadd5f32
LaBaguette299 <bogomolovnekit@yandex.ru>
2022-09-22 14:22:19 +0300

    Решение задания 1-1 #2

---
+++ Task's/Task_1-1/Task_1-1/Task_1-1.cpp
@@ -0,0 +1,50 @@
+#define _USE_MATH_DEFINES // for C++
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+/**
+ * \brief Функция расчёта значения "a" по заданной формуле.
+ * \param x Первый параметр функции.
+ * \param y Второй параметр функции.
+ * \param z Третий параметр функции.
+ */
+double searchA(double x, double y, double z);
+
+/**
+ * \brief Функция расчёта значения "b" по заданной формуле.
+ * \param y Первый параметр функции.
+ * \param z Второй параметр функции.
+ * \param a Третий параметр функции.
+ */
+double searchB(double y, double z, double a);
+
+/**
+ * \brief Точка входа в программу.
+ * \return Возвращает 0 в случае успеха.
+ */
+int main()
+{
+    const double x = -0.5;
+    const double y = 1.7;
+    const double z = 0.44;
+    
+    double a = searchA(x, y, z);
+    double b = searchB(y, z, a);
+    
+    cout << "Given: " << endl << "X = " << x << endl << "Y = " << y << endl << "Z = " << z << endl;
+    cout << "Answer: " << endl << "A = " << a << endl << "B = " << b << endl;
+    
+    return 0;
+}
+
+double searchA(double x, double y, double z)
+{
+    return exp(-y*z) * sin(x*y-z) - sqrt( abs(y*z+x) );
+}
+
+double searchB(double y, double z, double a)
+{
+    return y * sin(a * pow(z,2) * cos(2.0*z) - 1.0);
+}
